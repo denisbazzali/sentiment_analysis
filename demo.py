5450b7c2ea3e49f5260ae153a66c31e16d0aab3c
denis <denisbazzali@gmail.com>
2016-10-20 13:12:29 +0200

    Delete keys

--- demo.py
+++ demo.py
@@ -1,39 +1,52 @@
 from textblob import TextBlob
 import tweepy
 import csv
+import json
 
-consumer_key = 'n9yef3PmnYD6Yr7YTnODXswW4'
-consumer_secret = 'uLCkrM62ucxpbqVwEAH5sWkcnIwWsMgvjT9uzDrNn3FkjRAYVM'
 
-access_token = '287121416-6G1bB8c5X9c6NMTQwRTkacPgGtRmdV7JU8tOCPYq'
-access_token_secret = 'KxSfTVDIVkKtho8jbZguiUG3HXbluQENg6qw38xIPrjJC'
+consumer_key = ''
+consumer_secret = ''
+
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.search('Trump')
-
-for tweet in public_tweets:
-    print(tweet.text)
-    analysis = TextBlob(tweet.text)
-    print(analysis.sentiment)
+#public_tweets = api.search('Trump')
 
 
-def categorize(polarity):
-    if polarity <= -0.20:
-        return 'Negative'
-    if polarity >= 0.20:
-        return 'Positive'
-    return 'Neutral'
+file = open('today.txt', 'a')
+
+class CustomStreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        print(status.text)
+
+    def on_data(self, data):
+        json_data = json.loads(data)
+        # file.write(str(json_data['text']))
+        writeCSV(str(json_data['text']))
+
+sapi = tweepy.streaming.Stream(auth, CustomStreamListener())
+sapi.filter(track=['Trump'])
+
+# for tweet in public_tweets:
+#     print(tweet.text)
+#     analysis = TextBlob(tweet.text)
+#     print(analysis.sentiment)
 
 
-with open('tweets.csv', "w", newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
-    writer.writerow(("Username", "Tweet", "Polarity", "Classification"))
-    for tweet in public_tweets:
-        username = tweet.user.screen_name
-        polarity = TextBlob(tweet.text).sentiment.polarity
-        label = categorize(polarity)
-        writer.writerow((username, tweet.text, polarity, label))
+# def categorize(polarity):
+#     if polarity <= -0.20:
+#         return 'Negative'
+#     if polarity >= 0.20:
+#         return 'Positive'
+#     return 'Neutral'
+#
+#
+def writeCSV(tweet):
+    with open('trump.csv', "w", newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+        writer
