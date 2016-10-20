83d626975887ee5662f644cd96de38112bdd1ee6
denis <denisbazzali@gmail.com>
2016-10-20 11:18:07 +0200

    Sentiment Analysis

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 from textblob import TextBlob
 import tweepy
+import csv
 
 consumer_key = 'n9yef3PmnYD6Yr7YTnODXswW4'
 consumer_secret = 'uLCkrM62ucxpbqVwEAH5sWkcnIwWsMgvjT9uzDrNn3FkjRAYVM'
@@ -18,3 +19,21 @@ for tweet in public_tweets:
     print(tweet.text)
     analysis = TextBlob(tweet.text)
     print(analysis.sentiment)
+
+
+def categorize(polarity):
+    if polarity <= -0.20:
+        return 'Negative'
+    if polarity >= 0.20:
+        return 'Positive'
+    return 'Neutral'
+
+
+with open('tweets.csv', "w", newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+    writer.writerow(("Username", "Tweet", "Polarity", "Classification"))
+    for tweet in public_tweets:
+        username = tweet.user.screen_name
+        polarity = TextBlob(tweet.text).sentiment.polarity
+        label = categorize(polarity)
+        writer.writerow((username, tweet.text, polarity, label))
